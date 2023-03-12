72d49b492b5387a50e5d852022c0f440d3cbfc34
Arjan van Vught <arjan.van.vught@gmail.com>
2023-03-12 18:28:54 +0100

    V1.5
    - The network stack is not written in C++
    - Removed new uint8_t[FIRMWARE_MAX_SIZE]. There is no reason for putting this on the heap. It is even better for memory management to create the array in .bss -> out of memory check at link time.
    - Implemented progress indicator for the TFTP process.

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -7,41 +7,64 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
      NPROC=$(nproc)
 fi
 
+cd ..
+
 DIR=bootloader-tftp
-
-rm -rf /tmp/$DIR
-mkdir /tmp/$DIR
-
-cd ../$DIR
-
 MAKEFILE=Makefile*.GD32
 
-for m in $MAKEFILE
+array=('DP83848' 'RTL8201F' 'LAN8700')
+
+for f in $DIR
 do
-	make -f $m -j $NPROC clean
-	make -f $m -j $NPROC
-	retVal=$?
-			
-	if [ $retVal -ne 0 ]; then
-		echo "Error : " "$f" " : " "$m"
-		exit $retVal
-	fi
-			
-	SUFFIX=$(echo $m | cut -d '-' -f 2 | cut -d '.' -f 1)
+	rm -rf /tmp/$f
+	mkdir /tmp/$f
+	for i in "${array[@]}"
+	do
+		mkdir /tmp/$f/$i
+	done
+done
+
+for f in $DIR
+do
+	echo -e "\e[32m[$f]\e[0m"
+	if [ -d $f ]; then
+		cd "$f"	
 		
-	if [ $SUFFIX == 'Makefile' ]
-	then
-		cp gd32f4xx.bin /tmp/$DIR
-	else
-		echo $SUFFIX
-		mkdir /tmp/$DIR/$SUFFIX
-		cp gd32f4xx.bin /tmp/$DIR/$SUFFIX
-	fi		
+		for i in "${array[@]}"
+		do
+		
+			for m in $MAKEFILE
+			do
+				make -f $m -j $NPROC clean
+				make -f $m -j $NPROC ENET_PHY=$i
+				retVal=$?
+				
+				if [ $retVal -ne 0 ]; then
+				 	echo "Error : " "$f" " : " "$m"
+					exit $retVal
+				fi
+				
+				SUFFIX=$(echo $m | cut -d '-' -f 2 | cut -d '.' -f 1)
+			
+				if [ $SUFFIX == 'Makefile' ]
+				then
+					cp gd32f407.bin /tmp/$f/$i
+				else
+					echo $SUFFIX
+					mkdir /tmp/$f/$i/$SUFFIX/
+					cp gd32f407.bin /tmp/$f/$i/$SUFFIX
+				fi			
+			done
+			
+		done
+			
+		cd -
+	fi
 done
 
 cd /tmp/$DIR
 			
-find . -name gd32f4xx.bin | sort | xargs ls -al
-find . -name gd32f4xx.bin | xargs ls -al | wc -l
+find . -name gd32f407.bin | sort | xargs ls -al
+find . -name gd32f407.bin | xargs ls -al | wc -l
 
 cd -
